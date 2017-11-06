e24bfa769e8813202fd334203716ce529a5a91df
Arczaks <archaxwertq@gmail.com>
2017-11-06 23:54:45 +0100

    poprawki

--- src/generator/FXMLDocumentController.java
+++ src/generator/FXMLDocumentController.java
@@ -63,7 +63,7 @@ public class FXMLDocumentController implements Initializable {
    @FXML
     private void handleGenerateAction(ActionEvent event) {
         System.out.println("Generate");
-        String filename = "D:/NewExcelFile.xls" ;
+        String filename = "C:/NewExcelFile.xls" ;
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("FirstSheet"); 
         List<HSSFRow> rList = new ArrayList();
